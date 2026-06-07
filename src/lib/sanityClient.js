9e8cfc495cf45786ffa5ae20eaf6d3c0a7eb7e9e
farhanbin65 <samibin19@gmail.com>
2026-06-07 14:47:59 +0100

    sanity cdn false

--- src/lib/sanityClient.js
+++ src/lib/sanityClient.js
@@ -3,10 +3,10 @@ import imageUrlBuilder from '@sanity/image-url'
 
 export const client = createClient({
   projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
-  dataset: import.meta.env.VITE_SANITY_DATASET,
-  useCdn: true,
-  apiVersion: '2024-01-01',
-})
+  dataset:   import.meta.env.VITE_SANITY_DATASET,
+  useCdn:    false,
+  apiVersion: "2024-01-01",
+});
 
 const builder = imageUrlBuilder(client)
 export const urlFor = (source) => builder.image(source)
